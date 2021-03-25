4b6a38e199babc069113560bd1926b35c1619781
liam <liambabe96@gmail.com>
2021-03-25 13:08:08 +1000

    clear script

---
+++ ClearLogs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Clear old compress log files
+
+rm /var/log/auth.log.*.gz
+rm /var/log/daemon.log.*.gz
+rm /var/log/debug.*.gz
+rm /var/log/kern.log.*.gz
+rm /var/log/messages.*.gz
+rm /var/log/syslog.*.gz
+rm /var/log/user.log.*.gz
+rm /var/log/google-fluentd/google-fluentd.log.*.gz
+
+#Clear out caches packages
+
+apt-get clean
